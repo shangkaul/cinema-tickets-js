9d2e17b58a36b59e7f94f044c4033cd14f279afc
Shivang Kaul <shangkaul@Shivangs-MacBook-Air.local>
2025-07-15 13:17:26 +0100

    Runner main js created (for demo)

---
+++ main.js
@@ -0,0 +1,7 @@
+/**
+ * Main.js is a console runner file for the module
+ * Can be used for pre-limanary smoke tests and demo runs
+ * Just Run: 
+ *  node main.js
+ */
+
--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -1,3 +1,14 @@
+/* 
+ * JEST Enabled Test Suite for Test Driven Development
+The test suite helps test the code in accordance to the business rules and constraints given.
+Note: The Payment and Seat Reservation Services are not tested since as per assumptions those are out of scope for this assessment.
+So we assume those work well already.
+
+To run tests Excute in terminal:
+npm test
+*/
+
+
 import TicketService from "../src/pairtest/TicketService";
 import TicketTypeRequest from "../src/pairtest/lib/InvalidPurchaseException";
 import InvalidPurchaseException from "../src/pairtest/lib/TicketTypeRequest";
