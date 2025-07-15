7373807a8223ef10c8d4942059671613e8f81999
Shivang Kaul <shangkaul@Shivangs-MacBook-Air.local>
2025-07-15 12:42:25 +0100

    Init Test Suite using babel and jest

---
+++ test/TicketService.test.js
@@ -0,0 +1,10 @@
+import TicketService from "../src/pairtest/TicketService";
+import TicketTypeRequest from "../src/pairtest/lib/InvalidPurchaseException";
+import InvalidPurchaseException from "../src/pairtest/lib/TicketTypeRequest";
+
+
+describe('TicketService.purchaseTickets', ()=>{
+    it('Test for single adult reservation',()=>{
+        //Test to be written
+    })
+})
